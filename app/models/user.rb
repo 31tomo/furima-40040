81f6d3ba87c998b72b183c1556dd61b7a5d8ec17
31tomo <koba31tomo24@gmail.com>
2023-11-26 00:23:17 +0900

    passwordと名前カナ(全角)のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,16 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  validate :password_complexity
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
+
+  private
+
+  def password_complexity
+    # パスワードが英字と数字の両方を含むことを確認
+    unless password.match(/^(?=.*[a-zA-Z])(?=.*\d).+/)
+      errors.add :password, 'は英字と数字の両方を含めて設定してください'
+    end
+  end
 end
