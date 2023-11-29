5fa2c9479ec8e5de18a046c265c841de0a3cb1ef
31tomo <koba31tomo24@gmail.com>
2023-11-29 15:34:58 +0900

    出品ページ遷移と画像投稿機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :message, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true
 end
