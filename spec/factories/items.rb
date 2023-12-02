bbd6a236899062c16aacc4d23c0a9f3b41d65c04
31tomo <koba31tomo24@gmail.com>
2023-12-02 15:08:58 +0900

    商品一覧表示の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,8 +10,8 @@ FactoryBot.define do
     price {Faker::Number.between(from: 300, to: 9_999_999)}
     association :user
 
-    after(:build) do |message|
-      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
