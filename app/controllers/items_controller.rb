6b0778746c80c5657cb3ca1348114167d85be81f
31tomo <koba31tomo24@gmail.com>
2023-12-03 20:33:00 +0900

    削除の実装と削除後トップページへ遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :contributor_confirmation, only: [:edit]
+  before_action :contributor_confirmation, only: [:edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
 
   def new
@@ -34,6 +34,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+  
   private
   def item_params
     params.require(:item).permit(:item_name, :price, :message, :image, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
